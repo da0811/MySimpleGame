439c8b0d3381170777c6df73dfb5a47111ef0ac9
Nick Morales <54779299+nickmo12@users.noreply.github.com>
2021-11-13 19:35:56 -0500

    fixed arrow shooting and added target

--- src/MySimpleGame.java
+++ src/MySimpleGame.java
@@ -1,8 +1,5 @@
-import com.sun.xml.internal.messaging.saaj.packaging.mime.util.QPEncoderStream;
-
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.util.Random;
 
 public class MySimpleGame extends GamePanel {
 
@@ -10,37 +7,71 @@ public class MySimpleGame extends GamePanel {
 
     Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 
+
     Image grassLand = Toolkit.getDefaultToolkit().getImage("./images/grass_template_2.JPG");
     Image cabin = Toolkit.getDefaultToolkit().getImage("./images/woodcutter_cabin.PNG");
+    Image target = Toolkit.getDefaultToolkit().getImage("./images/target_side_view.png");
+
 
     Sprite ranger1 = new Sprite(100, 100, (int)(screen.width*.5), (int)(screen.height*.5),"rg", Ranger.pose, 10, "PNG" );
 
-    int cabinX = (int)(screen.width * .5);
-
-    int cabinY = (int)(screen.height * .5);
-
-    int cabinWidth = (int)(screen.width * .33);
-
-    int cabinHeight = (int)(screen.height * .33);
-
+    int cabinX = (int)(screen.width / 2);
+    int cabinY = (int)(screen.height / 2);
+    int cabinWidth = (int)(screen.width / 3);
+    int cabinHeight = (int)(screen.height / 3);
     int cabinWidthOffset = cabinX+cabinWidth;
 
     int mx = 0;
-
     int my = 0;
 
     int nx = 0;
-
     int ny = 0;
 
-    Line cabinDoor = new Line(cabinX + 300, cabinY + 240, cabinX + 340, cabinY + 255);
+    //Line cabinDoor = new Line(cabinX + (cabinWidth/2) - (cabinWidth / 16),  cabinY + (cabinHeight/2) + (cabinHeight/8), cabinX + (cabinWidth/2) + (cabinWidth/80) , cabinY + (cabinHeight/2) + (cabinHeight/6));
+    //Line topCabin = new Line(cabinX, cabinY, cabinX + cabinWidth, cabinY);
+    //Line testLine = new Line(cabinX, cabinY, cabinX + cabinWidth/2, cabinY);
+    //Line testLine2 = new Line(cabinX + cabinWidth/2, cabinY, cabinX + cabinWidth/2, cabinY + cabinHeight/2);
 
-    Arrow arrow = new Arrow(-1000, 0, 0,"arrow.png");
+    //Arrow arrow1, arrow2;
+    Arrow[] arrows = new Arrow[5];
+    int nextArrow = 0;
 
 
     public void initialize() {
         //mySprite = ranger1;
+        for(int i = 0; i < arrows.length; i++){
+            arrows[i] = new Arrow(0, i * 10, 0);
+            System.out.println("arrow" + i);
+        }
 
+    }
+    public void paint(Graphics gfx) {
+        gfx.setColor(new Color(100, 100, 100));
+
+        gfx.fillRect(0, 0, screen.width, screen.height);
+
+        gfx.drawImage(grassLand, 0, 0, screen.width, screen.height, null);
+
+        gfx.drawImage(cabin, cabinX, cabinY, cabinWidth, cabinHeight, null);
+        gfx.drawImage(target, screen.width - (screen.width / 5), (screen.height / 2) - (screen.height/3), target.getWidth(null)/2, target.getHeight(null)/2, null);
+
+        //gfx.drawImage(target, screen.width - (screen.width/5), )
+
+        ranger1.draw(gfx);
+
+
+
+        gfx.setColor(Color.BLUE);
+//        topCabin.draw(gfx);
+//        gfx.setColor(Color.magenta);
+//        testLine.draw(gfx);
+//        testLine2.draw(gfx);
+
+        gfx.setColor(Color.red);
+        //cabinDoor.draw(gfx);
+        for(int i = 0; i < arrows.length; i++) {
+            arrows[i].draw(gfx);
+        }
     }
 
     @Override
@@ -53,50 +84,32 @@ public class MySimpleGame extends GamePanel {
 
             if(pressing[SHIFT]) speed = 2;
             else                speed = 1;
-            if(pressing[F]) { ranger1.shoot(arrow);
+            if(pressing[F]){
+                    //ranger1.shoot(arrows[nextArrow]);
+
+                arrows[nextArrow].fire(ranger1.px, ranger1.py, 0, 10);
+                System.out.println("Arrow " + nextArrow + " fired.");
+
+                pressing[F] = false;
+                nextArrow++;
 
 
+                if(nextArrow == arrows.length) {
+                    nextArrow = 0;
+                }
             }
-
         }
 
         if(pressing[SPACE]) ranger1.isAlive = false;
         if(pressing[COMMA]) ranger1.revive();
     }
 
-    public void paint(Graphics gfx) {
-        gfx.setColor(new Color(100, 100, 100));
-
-        gfx.fillRect(0, 0, screen.width, screen.height);
-
-        gfx.drawImage(grassLand, 0, 0, screen.width, screen.height, null);
-
-        gfx.drawImage(cabin, cabinX, cabinY, cabinWidth, cabinHeight, null);
-
-        ranger1.draw(gfx);
-        arrow.draw(gfx);
-
-        gfx.setColor(Color.red);
-        cabinDoor.draw(gfx);
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
-    }
-
-    public void mouseMoved(MouseEvent event) {
-        mx = event.getX();
-        my = event.getY();
-    }
-
-
     @Override
     public void move_Computer_Controlled_Entities() {
         // enemies, fauna
-
-        arrow.move();
-
+        for (int i = 0; i < arrows.length; i++) {
+            arrows[i].move();
+        }
     }
 
     @Override
@@ -123,6 +136,15 @@ public class MySimpleGame extends GamePanel {
     @Override
     public void mouseExited(MouseEvent e) {
 
+    }
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    public void mouseMoved(MouseEvent event) {
+        mx = event.getX();
+        my = event.getY();
     }
 
 
